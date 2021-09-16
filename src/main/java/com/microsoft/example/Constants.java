bfb383515cf226f7213bbd1c9362b35f68ffc3b7
Tiago Pascoal <tiago@pascoal.net>
2021-09-16 12:11:10 +0100

    Add constants

---
+++ src/main/java/com/microsoft/example/Constants.java
@@ -0,0 +1,8 @@
+package com.microsoft.example;
+
+public class Constants
+{
+	public static final String DEFAULT_CURRENCY = "USD";	
+	public static final String ADMIN_NAME = "admin";
+	public static final String GH_TOKEN = "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";	
+}
